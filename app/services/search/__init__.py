b3d92daebc5a03278bbbb1d2850b638299b41d80
Somil Singhai <somilsinghai1804@gmail.com>
2025-10-09 19:28:05 +0530

    solved N+1 query issue and refactored code

---
+++ app/services/search/__init__.py
@@ -0,0 +1,21 @@
+from .direct_search import DirectFlightSearch
+from .one_stop_search import OneStopFlightSearch
+from .two_stop_search import TwoStopFlightSearch
+from .itinerary_builder import ItineraryBuilder
+from .helpers import (
+    fetch_flight_instances_bulk,
+    is_valid_connection,
+    create_flight_leg_from_instance,
+    index_instances_by_route,
+)
+
+__all__ = [
+    'DirectFlightSearch',
+    'OneStopFlightSearch',
+    'TwoStopFlightSearch',
+    'ItineraryBuilder',
+    'fetch_flight_instances_bulk',
+    'is_valid_connection',
+    'create_flight_leg_from_instance',
+]
+
